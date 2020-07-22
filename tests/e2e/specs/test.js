2aacb2548bfc9c597e53121293cc7d03e7d6b0ee
Mitchell Caisse <mitchell@mwcaisse.com>
2020-07-22 00:38:26 -0400

    Added cypress test to verify that unauthorized users are redirected to login page

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,21 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+describe("Authentication Flow", () => {
+
+  it("Automatically navigates to login page if not logged in", () => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "/api/user/me",
+      status: 401
+    }).as("getCurrentUser")
+
+    cy.visit("/");
+
+    cy.wait("@getCurrentUser");
+
+    cy.url().should("equal", Cypress.config().baseUrl + "login");
+
+  });
+
+});
